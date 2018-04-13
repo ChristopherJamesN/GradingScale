066e179d6e6628541592d160378f8a67fc970074
ChristopherJamesN <nady.christopher@gmail.com>
2018-04-12 20:18:20 -0500

    Initial commit.

---
+++ GradingScale/GradingScale/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  GradingScale
+//
+//  Created by Christopher Nady on 4/12/18.
+//  Copyright © 2018 Nady Analytics, LLC. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
