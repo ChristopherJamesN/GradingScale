66b7fe52a7ffa388e53c441d6e0ab481210c40ff
ChristopherJamesN <nady.christopher@gmail.com>
2018-04-13 19:53:57 -0500

    Add dismiss keyboard function to grade entry class.

--- GradingScale/GradingScale/GradeEntryViewController.swift
+++ GradingScale/GradingScale/GradeEntryViewController.swift
@@ -12,13 +12,26 @@ class GradeEntryViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //Looks for single or multiple taps.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(GradeEntryViewController.dismissKeyboard))
+        
+        //Uncomment the line below if you want the tap not not interfere and cancel other interactions.
+        //tap.cancelsTouchesInView = false
+        
+        view.addGestureRecognizer(tap)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //Calls this function when the tap is recognized.
+    @objc func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+
 
 
 }
